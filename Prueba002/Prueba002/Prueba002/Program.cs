f33802331f0da5459b2f872a67ac611d8d8b4cea
Abel De Jesus Guzman Cardenas <abeldejg2005@gmail.com>
2023-11-20 16:18:08 -0600

    Por fin termine la funcionalidad toca ahora diseño gracias :D

--- Prueba002/Prueba002/Prueba002/Program.cs
+++ Prueba002/Prueba002/Prueba002/Program.cs
@@ -16,6 +16,12 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole<int>>(options => opti
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.LogoutPath = "/Identity/Account/Logout";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
 
 var app = builder.Build();
 
